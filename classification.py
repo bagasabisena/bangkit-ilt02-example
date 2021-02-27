98b4855e19c48d781e0f35307a93fb028277f494
Bagas Abisena <bagas.abisena@gojek.com>
2021-02-27 08:52:44 +0700

    load digits dataset

---
+++ classification.py
@@ -0,0 +1,6 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+X, y = load_digits(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+print(X_train, X_train.shape)
